7200ca95e9462c9a71dbca763a9f3785c068f9db
BlaSSSTTT <antonchyk.vladyslav@gmail.com>
2024-03-20 21:18:36 +0200

    fix bugs

--- lvl1.1/Practice1.js
+++ lvl1.1/Practice1.js
@@ -8,8 +8,8 @@ let Product = function(ID, name, description, price, brand, quantity){
     this.activeSize;
     this.quantity = quantity;
     this.date = Date();
-    this.reviews;
-    this.images;
+    this.reviews = [];
+    this.images = [];
     this.getID = function(){
         return ID;
     }
@@ -74,46 +74,48 @@ let Product = function(ID, name, description, price, brand, quantity){
 
 
     this.getReviewByID = function(id){
-        reviews.forEach(element => {
-            if(element.getID === id){
+        for (const element of this.reviews) {
+            console.log(element);
+            if (element.id === id) {
                 return element;
             }
-            return null;
-        });
+        }
+        return null; 
     }
     this.addSize = function(size){
-        this.sizes.push(size);
+        this.sizes[this.sizes.length]= size;
     }
-    this.deleteSize = function(id){
-        this.sizes.slice(id,1);
+    this.deleteSize = function(el){
+        let id = this.sizes.indexOf(el);
+        this.sizes.splice(id,1);
     }
     this.addReview = function(review){
-        this.reviews.push(review);
+        this.reviews[this.reviews.length] = review;
     }
     this.deleteReview = function(id){
         let i = 0;
-        for(;i<reviews.size;i++){
-            if(element.getID === id){
+        for(;i<this.reviews.length;i++){
+            if(this.reviews[i].id == id){
+                console.log(i);
                 break;
             }
         }
-        this.reviews.slice(i,1);
+        this.reviews.splice(i,1);
     }
     this.getAverageRating = function(){
         let sum = 0;
-        reviews.forEach(element => {
+        for (const element of this.reviews) {
             sum+=element.rating['service'];
             sum+=element.rating['price'];
             sum+=element.rating['value'];
             sum+=element.rating['quality'];
-        });
-        return sum/reviews.size/4;
+        }
+        return sum/this.reviews.length/4;
     }
 }
 
-
 let Review = function(ID, author,data, comment,rating){
-    this.ID = ID;
+    this.id = ID;
     this.author = author;
     this.data = data;
     this.comment = comment;
@@ -123,3 +125,22 @@ let Review = function(ID, author,data, comment,rating){
 
 
 let pr = new Product(0,"name","description","price","brand",12);
+
+pr.setDate(new Date);
+pr.setActiveSize('S');
+let rating = [];
+rating['service'] = 5;
+rating['price'] = 3;
+rating['value'] = 4;
+rating['quality'] = 1;
+
+let review = new Review(0,"me",new Date,"blablabla",rating);
+pr.addReview(review);
+console.log(pr.reviews);
+console.log("review "+pr.getReviewByID(0).comment);
+pr.addSize("XXXL");
+pr.deleteSize("XXL");
+console.log("Everage: "+pr.getAverageRating());
+console.log(pr.sizes);
+pr.deleteReview(0);
+console.log(pr.reviews);
